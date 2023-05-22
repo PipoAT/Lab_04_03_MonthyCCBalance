cc406617c77cab5312484020d9a6588aa6f3adee
Andrew Pipo <atpipo2026@gmail.com>
2023-05-22 16:52:46 -0400

    Completed Task 3 of Part II

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,24 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // declare variables for initial balance, interest rate, and new balance
+        double initialBalance = 5000.00;
+        double interestRate = 17.00; // 17% interest rate per month
+        double interestPayment = 0;
+        double newBalance = 0;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        newBalance = initialBalance; // set up the initial balance
+
+        // calculate the interest rate newBalance at the interest rate for the next 2 months
+        for (int i = 1; i < 3; i++) {
+            interestPayment = (newBalance * (interestRate / 100));
+            newBalance = newBalance + interestPayment;
+
+            // output the interest amount and new balance after each month
+            System.out.println("Your interest amount (in USD) after " + i + " month(s) is: " + interestPayment);
+            System.out.println("Your new balance (in USD) after " + i + " month(s) is: " + newBalance);
+
         }
     }
 }
